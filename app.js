ec8d8c158ea806f67ba089bc3780397e6780f3ae
Ellen <ellen31725@gmail.com>
2023-08-26 17:18:33 +0800

    feat: modify patient select

--- app.js
+++ app.js
@@ -30,6 +30,15 @@ app.engine(
           return options.inverse(this);
         }
       },
+      select: function (value, options) {
+        return options.fn(this)
+          .split('\n')
+          .map(function (v) {
+            var t = 'value="' + value + '"'
+            return !RegExp(t).test(v) ? v : v.replace(t, t + ' selected="selected"')
+          })
+          .join('\n')
+      }
     },
   })
 );
--- models/seeds/patientSeeder.js
+++ models/seeds/patientSeeder.js
@@ -2,34 +2,34 @@ const db = require('../../config/mongoose')
 const Patient = require('../patient')
 const LocationCategory = require('../locationCategory')
 
-  const SEED_PATIENT = [
-    {
-      chartNo:'111',
-      name: '王大明',
-      gender: "male",
-      age: 38,
-      wardNumber:'606',
-      locationCategory:'inpatientA'
-    },
-    {  
-       chartNo:'222',
-       name: '許小芬',
-       gender: "Female",
-       age: 28,
-       wardNumber:'305',
-       locationCategory:'postpartum'
-    },
-    {
-       chartNo:'333',
-       name: '翁添文',
-       gender: "male",
-       age: 60,
-       wardNumber:'203',
-       locationCategory:'rcw'
-    }
-  ]
+const SEED_PATIENT = [
+  {
+    chartNo: '111',
+    name: '王大明',
+    gender: "Male",
+    age: 38,
+    wardNumber: '606',
+    locationCategory: 'inpatientA'
+  },
+  {
+    chartNo: '222',
+    name: '許小芬',
+    gender: "Female",
+    age: 28,
+    wardNumber: '305',
+    locationCategory: 'postpartum'
+  },
+  {
+    chartNo: '333',
+    name: '翁添文',
+    gender: "Male",
+    age: 60,
+    wardNumber: '203',
+    locationCategory: 'rcw'
+  }
+]
 
- db.once('open', async () => {
+db.once('open', async () => {
   console.log('start patient seeder!')
   // 由於 category ID 是隨機產生，必須要先把以新增的分類取出，供後續新增 patient 時 categoryId 使用
   const locationCategories = await LocationCategory.find()
@@ -39,18 +39,18 @@ const LocationCategory = require('../locationCategory')
       ...prev,
       [curr.locationCategory]: curr._id
     }
-  }, {}) 
+  }, {})
 
   Promise.all(SEED_PATIENT.map((patient) => {
-      return Patient.create({
-         chartNo: patient.chartNo,
-         name: patient.name,
-         gender: patient.gender,
-         age:patient.age,
-         wardNumber: patient.wardNumber,
-         locationCategoryId: locationCategoryIdMapping[patient.locationCategory]
-      })
-    }))
+    return Patient.create({
+      chartNo: patient.chartNo,
+      name: patient.name,
+      gender: patient.gender,
+      age: patient.age,
+      wardNumber: patient.wardNumber,
+      locationCategoryId: locationCategoryIdMapping[patient.locationCategory]
+    })
+  }))
     .then(() => {
       console.log('patient seeder done')
       db.close()
